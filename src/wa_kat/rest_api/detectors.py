18dce6ede35af5d48718c12109b2afeefb1d8070
Bystroushaak <bystrousak@kitakitsune.org>
2015-07-01 01:13:49 +0200

    Added template files for some of the components.

---
+++ src/wa_kat/rest_api/detectors.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+def detect_title(url):
+    pass
+
+
+def detect_author(url):
+    pass
+
+
+def detect_keywords(url):
+    pass
+
+
+def detect_creation_date(url):
+    pass
+
+
+def detect_language(url):
+    pass
+
+
+def detect_place(url):
+    pass
+
+
+def detect_annotation(url):
+    pass
