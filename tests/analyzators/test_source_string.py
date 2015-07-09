731fa8cbd96c1e3efe2ade77692980acd3ecd5f8
Bystroushaak <bystrousak@kitakitsune.org>
2015-07-10 00:18:32 +0200

    Added tests of the SourceString.

---
+++ tests/analyzators/test_source_string.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from wa_kat.analyzators.source_string import SourceString
+
+
+# Tests =======================================================================
+def test_source_string():
+    assert SourceString("azgabash") == "azgabash"
+    assert SourceString("azgabash", source="DC") == "azgabash"
+
+    assert "DC" in SourceString("azgabash", source="DC").__repr__()
