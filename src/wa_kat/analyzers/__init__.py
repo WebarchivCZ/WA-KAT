b74483cf25503f17b0d6fffd8082c8c6f44c4fa4
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-15 05:50:17 +0100

    Import of analyzers added to analyzers/__init__.py.

--- src/wa_kat/analyzers/__init__.py
+++ src/wa_kat/analyzers/__init__.py
@@ -4,3 +4,9 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from title_detector import get_title_tags
+from place_detector import get_place_tags
+from keyword_parser import get_keyword_tags
+from author_detector import get_author_tags
+from language_detector import get_lang_tags
+from annotation_detector import get_annotation_tags
