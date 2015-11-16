2b1e0467a00567631fb86ba7ecd9925b35e726a1
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-16 22:51:13 +0100

    #23: Added headers for the cooperative part.

--- src/wa_kat/rest_api/__init__.py
+++ src/wa_kat/rest_api/__init__.py
@@ -4,17 +4,24 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from os.path import join
 
+from bottle import post
+from bottle_rest import form_to_params
 
 
 # Variables ===================================================================
-
+API_PATH = "api_v1/"
 
 
 # Functions & classes =========================================================
+@post(join(API_PATH, "register"))
+@form_to_params
+def register(url):
+    pass
 
 
-
-# Main program ================================================================
-if __name__ == '__main__':
+@post(join(API_PATH, "result"))
+@form_to_params
+def get_result(url):
     pass
