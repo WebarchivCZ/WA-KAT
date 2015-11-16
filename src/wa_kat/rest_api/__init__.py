99f0dc4b641fb0e009d2293e7c49c8caa994a02f
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-16 22:54:56 +0100

    #23: get_result() changed from POST to GET request.

--- src/wa_kat/rest_api/__init__.py
+++ src/wa_kat/rest_api/__init__.py
@@ -6,6 +6,7 @@
 # Imports =====================================================================
 from os.path import join
 
+from bottle import get
 from bottle import post
 from bottle_rest import form_to_params
 
@@ -21,7 +22,7 @@ def register(url):
     pass
 
 
-@post(join(API_PATH, "result"))
+@get(join(API_PATH, "result"))
 @form_to_params
 def get_result(url):
     pass
