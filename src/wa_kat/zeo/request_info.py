3d80d7136dae24d5abf573bbde6965381d604e70
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-21 23:47:40 +0100

    #23: Added docstrings (#25) for PropertyInfo and _get_req_mapping().

--- src/wa_kat/zeo/request_info.py
+++ src/wa_kat/zeo/request_info.py
@@ -20,8 +20,46 @@ from worker import worker
 
 
 # Functions & classes =========================================================
+class PropertyInfo(namedtuple("PropertyInfo", ["name",
+                                               "filler_func",
+                                               "filler_params"])):
+    """
+    This class is used to join one function (:attr:`filler_func`) from
+    :mod:`.analyzers` with its parameters (:attr:`filler_params`) and to say,
+    where it should be stored (:attr:`name`).
+
+    Example::
+
+        PropertyInfo(
+            name="title_tags",
+            filler_func=analyzers.get_title_tags,
+            filler_params=lambda self: self.index,
+        )
+
+    Attributes:
+        name (str): Name of the property, where the data should be stored.
+        filler_func (fn ref): Reference to function, which will be called.
+        filler_params (lambda self:): Lambda function, which returns the
+            parameters for :attr:`filler_func`. Function takes one parameter,
+            which contains the reference to :class:`.PropertyInfo` object in
+            which the result is stored.
+    """
+
+
 @lru_cache()
 def _get_req_mapping():
+    """
+    This function serves as joiner for functions from :mod:`analyzers`, to map
+    them to properties, which will be stored in :class:`.RequestInfo` database
+    object.
+
+    I've decided to do it this way, because it will allow paralel processing
+    of the properties/functions and also all configuration is at one place,
+    instead of multiple places in object properties / methods and so on.
+
+    Returns:
+        OrderedDict: with :class:`.PropertyInfo` objects.
+    """
     REQ_MAPPING = [
         PropertyInfo(
             name="title_tags",
@@ -60,18 +98,11 @@ def _get_req_mapping():
         ),
     ]
 
+    # construct the OrderedDict - I want the order preserved
     return OrderedDict(
         (req.name, req)
         for req in REQ_MAPPING
     )
-
-
-class PropertyInfo(namedtuple("PropertyInfo", ["name",
-                                               "filler_func",
-                                               "filler_params"])):
-    """
-    TODO: docstrings
-    """
 
 
 class RequestInfo(Persistent):
@@ -130,7 +161,7 @@ class RequestInfo(Persistent):
             "values": {
                 property_name: [
                     tag.to_dict()
-                    for tag in getattr(self, property_name)
+                    for tag in getattr(self, property_name) or []
                 ]
                 for property_name in _get_req_mapping().keys()
             }
