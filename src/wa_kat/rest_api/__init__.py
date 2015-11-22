9a6d8c804f86a6c934daf95e8e34afcca79d360a
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-22 23:31:56 +0100

    #23: Added sketch of the REST API.

--- src/wa_kat/rest_api/__init__.py
+++ src/wa_kat/rest_api/__init__.py
@@ -4,6 +4,7 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import time
 from os.path import join
 
 from bottle import get
@@ -18,13 +19,27 @@ API_PATH = "api_v1/"
 
 
 # Functions & classes =========================================================
+
+
+
 @post(join(API_PATH, "register"))
 @form_to_params
-def register(url):
-    pass
-
-
-@get(join(API_PATH, "result"))
-@form_to_params
 def get_result(url):
-    pass
+    rd = RequestDatabase()
+
+    # handle cacheing
+    try:
+        ri = rd.get_request(url)
+
+        if ri.is_old():
+            ri.paralel_processing()
+    except Exception as e:
+        return {
+            "status": False,
+            "error": e.message
+        }
+
+    return {
+        "status": True,
+        "body": ri.to_dict()
+    }
