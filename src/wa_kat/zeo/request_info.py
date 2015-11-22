c03ef27f6172a44a1b0e1f92cb13673d82d9d49c
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-22 03:10:57 +0100

    #25: Added docstrings for requests_info.py (#23).

--- src/wa_kat/zeo/request_info.py
+++ src/wa_kat/zeo/request_info.py
@@ -108,7 +108,35 @@ def _get_req_mapping():
 
 @total_ordering
 class RequestInfo(Persistent):
+    """
+    This object is used to hold informations about requests, which are
+    processed at that moment. It stores all its properites in ZODB.
+
+    The object is basically used as cache AND progress bar indicator of work.
+
+    Attributes:
+        url (str): URL to which this info object is related.
+        domain (str): Domain of the URL.
+        index (str): Content of the index page of URL.
+        creation_ts (float): Timestamp of object creation.
+        downloaded_ts (float): Timestamp showing where the :attr:`index` was
+            downloaded.
+        processing_started_ts (float): Timestamp showing when the processing of
+            additional properties started.
+        processing_ended_ts (float): Timestamp showing when the processing of
+            additional properties stopped.
+
+    Warning:
+        There is more properties, one for all analyzator function from
+        :mod:`.analyzers`.
+    """
     def __init__(self, url):
+        """
+        Constructor.
+
+        Args:
+            url (str): URL to which this request is related.
+        """
         self.url = url
         self.domain = urlparse(url).netloc
         self.index = None
@@ -123,12 +151,29 @@ class RequestInfo(Persistent):
             setattr(self, req.name, None)
 
     def _download(self, url):
+        """
+        Download `url` and return it as utf-8 encoded text.
+
+        Args:
+            url (str): What should be downloaded?
+
+        Returns:
+            str: Content of the page.
+        """
         resp = requests.get(url)  # TODO: custom headers
 
-        return resp.text.encode("utf-8")
+        return resp.text.encode("utf-8")  # TODO: what about binaries?
 
     @transaction_manager
     def paralel_processing(self, client_conf=None):
+        """
+        Lauch paralel processes (see :func:`.worker`) to fill properties with
+        data.
+
+        Args:
+            client_conf (str, default None): Optional parameter used by tests
+                to redirect the database connections to test's environment.
+        """
         self.index = self._download(self.url)
         self.downloaded_ts = time.time()
         self.processing_started_ts = time.time()
@@ -148,6 +193,13 @@ class RequestInfo(Persistent):
 
     @transaction_manager
     def _get_all_set_properties(self):
+        """
+        Collect names of set properties.
+
+        Returns:
+            set: Set containing names of all properties, which are set to \
+                 non-None value.
+        """
         return set(
             property_name
             for property_name in _get_req_mapping().keys()
@@ -155,13 +207,34 @@ class RequestInfo(Persistent):
         )
 
     def progress(self):
+        """
+        Get progress.
+
+        Returns:
+            tuple: (int(done), int(how_many))
+        """
         return len(self._get_all_set_properties()), len(_get_req_mapping())
 
     def is_all_set(self):
+        """
+        Is all properties set?
+
+        Returns:
+            bool: True if they are set.
+        """
         return self._get_all_set_properties() == set(_get_req_mapping().keys())
 
     @transaction_manager
     def to_dict(self):
+        """
+        This method is used in with connection to REST API. It basically
+        converts all important properties to dictionary, which may be used by
+        frontend.
+
+        Returns:
+            dict: ``{"all_set": bool, "progress": [int(done), int(how_many)], \
+                  "values": {"property": [values], ..}}``
+        """
         return {
             "all_set": self.is_all_set(),
             "progress": self.progress(),
@@ -184,6 +257,13 @@ class RequestInfo(Persistent):
         return float(self.creation_ts).__lt__(obj.creation_ts)
 
     def _p_resolveConflict(self, oldState, savedState, newState):
+        """
+        This method is used to resolve conflicts in ZODB, when two processes
+        create two mismatched transactions. Which actually happens all the
+        time.
+
+        Standard ZODB stuff, just google it.
+        """
         # for native properties ..
         native_properties = {
             "url",
