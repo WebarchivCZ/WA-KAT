d4748473bd523c29e79e4d8af19d5cce21c52472
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-22 02:48:50 +0100

    #25: Added docstrings for worker.py (#23).

--- src/wa_kat/zeo/worker.py
+++ src/wa_kat/zeo/worker.py
@@ -10,14 +10,31 @@ import transaction
 
 
 # Functions & classes =========================================================
-def worker(url, property_info, filler_params, conf_path=None):
+def worker(url_key, property_info, filler_params, conf_path=None):
+    """
+    This function is meant to run as process on the background.
+
+    It runs ``property_info.filler_func(*filler_params)`` and then stores them
+    in ZODB under `url_key`.
+
+    Warning:
+        This function doesn't return the data, but puts them to database!
+
+    Args:
+        url_key (str): Key which will be used for database lookup.
+        property_info (obj): :class:`.PropertyInfo` instance.
+        filler_params (list): List of parameters for function which will be
+            called to retreive data.
+        conf_path (str, default None): Optional parameter used by tests to
+            redirect the database connections to test's environment.
+    """
     from .request_database import RequestDatabase
 
     # this may take some time, hence outside transaction manager
     data = property_info.filler_func(*filler_params)
 
     db = RequestDatabase(conf_path=conf_path)
-    req = db.get_request(url)
+    req = db.get_request(url_key)
 
     with transaction.manager:
         val = getattr(req, property_info.name)
