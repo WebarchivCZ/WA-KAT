b19b6562ed6815a226450a9968de5f49727d32da
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-22 02:31:05 +0100

    #22: Added test_RequestInfo_paralel_download().

--- tests/zeo/test_request_database.py
+++ tests/zeo/test_request_database.py
@@ -4,8 +4,9 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-import pytest
+import time
 
+import pytest
 import transaction
 
 from wa_kat.zeo import RequestDatabase
@@ -28,6 +29,19 @@ def request_info(client_conf_path):
     # make sure, that new object is created and thus it is indeed new
     # connection to database
     return rdb(client_conf_path).get_request(TEST_URL)
+
+
+# Functions ===================================================================
+def circuit_breaker_progress_retry(ri, attempts=20):
+    for i in range(attempts):
+        try:
+            print ri.progress()
+            assert ri.is_all_set()
+            return
+        except AssertionError:
+            time.sleep(1)
+
+    raise IOError("Paralel processing took too much time, aborting.")
 
 
 # Tests =======================================================================
@@ -54,10 +68,10 @@ def test_worker(rdb, request_info, client_conf_path):
 
     property_info = _get_req_mapping()["title_tags"]
     worker(
-        request_info.url,
-        property_info,
-        property_info.filler_params(request_info),
-        client_conf_path,  # to monkeypatch the path to the zeo client conf
+        url=request_info.url,
+        property_info=property_info,
+        filler_params=property_info.filler_params(request_info),
+        conf_path=client_conf_path,  # to monkeypatch the zeo client conf path
     )
 
     # lookup to the request info properties
@@ -71,6 +85,31 @@ def test_worker(rdb, request_info, client_conf_path):
     assert isinstance(request.title_tags, list)
 
 
-def test_RequestInfo_paralel_download(request_info):
-    assert request_info.url == TEST_URL
-    assert not request_info.index
+def test_RequestInfo_paralel_download(request_info, client_conf_path):
+    with transaction.manager:
+        assert request_info.url == TEST_URL
+        assert not request_info.index
+        assert not request_info.is_all_set()
+        assert request_info.progress()[0] == 1  # set from test_worker()
+
+    request_info.paralel_processing(client_conf=client_conf_path)
+
+    # wait until the processing of all requests is finished
+    circuit_breaker_progress_retry(request_info, attempts=20)
+
+    with transaction.manager:
+        assert request_info.url
+        assert request_info.domain
+        assert request_info.index
+        assert request_info.creation_ts
+        assert request_info.downloaded_ts
+        assert request_info.processing_started_ts
+        assert request_info.processing_ended_ts
+
+        assert request_info.is_all_set()
+        assert request_info.progress()[0] == request_info.progress()[1]
+
+        set_keys = set(request_info.to_dict()["values"].keys())
+        assert set_keys == set(_get_req_mapping().keys())
+
+        assert request_info.to_dict()["values"]["place_tags"]
