e8acf6cd1ef18b35c4e1d17495258d5b07fb4626
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-25 12:28:00 +0100

    #32: Added basic logic for the REST API querying, error log and so on.

--- src/wa_kat/templates/static/py/wa_kat_main.py
+++ src/wa_kat/templates/static/py/wa_kat_main.py
@@ -4,12 +4,74 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-from browser import alert
+import json
+
+from browser import ajax
+from browser import alert  # TODO: Remove
+from browser import document
 
 
 # Variables ===================================================================
-
-
-
 # Functions & classes =========================================================
-alert(1)
+def show_url_box_error(msg):
+    document["urlbox_error"].innerHTML = msg
+    document["whole_urlbox_error"].style.display = "block"
+
+
+def hide_url_box_error():
+    document["whole_urlbox_error"].style.display = "none"
+
+
+def fill_inputs(values):
+    alert(values)
+
+
+def show_progress(progress):
+    pass
+
+
+def on_complete(req):
+    if not (req.status == 200 or req.status == 0):
+        show_url_box_error(req.text)
+        return
+
+    resp = json.loads(req.text)
+
+    if not resp["status"]:
+        show_url_box_error(resp["error"])
+        return
+
+    if not resp["body"]["all_set"]:
+        show_progress(resp["body"]["progress"])
+        make_request(document["url"].value)
+        return
+
+    show_progress(resp["body"]["progress"])
+    fill_inputs(resp["body"]["values"])
+
+
+def make_request(url):
+    req = ajax.ajax()
+    req.bind('complete', on_complete)
+    req.open('POST', "/api_v1/analyze", True)
+    req.set_header('content-type', 'application/x-www-form-urlencoded')
+    req.send({'url': url})
+
+
+def start_analysis(ev):
+    url = document["url"].value.strip()
+
+    if not url.strip():
+        show_url_box_error("URL musí být vyplněna.")
+        return
+
+    hide_url_box_error()
+
+    if not (url.startswith("http://") or url.startswith("http://")):
+        url = "http://" + url
+        document["url"].value = url
+
+    make_request(url)
+
+
+document["run_button"].bind("click", start_analysis)
