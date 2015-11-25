25d2d869c4dcbda9fe36dc9b4be256b3bc6b2467
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-25 03:44:22 +0100

    Added extended REST tests. Fixed #23.

--- tests/rest/test_rest.py
+++ tests/rest/test_rest.py
@@ -9,24 +9,30 @@ import pytest
 import requests
 
 
-# Variables ===================================================================
-
-
-
 # Fixtures ====================================================================
-# @pytest.fixture
-# def fixture():
-#     pass
-
-# with pytest.raises(Exception):
-#     raise Exception()
+@pytest.fixture
+def analyze_url(web_port):
+    return "http://localhost:%d/api_v1/analyze" % web_port
 
 
 # Tests =======================================================================
-def test_rest(web_port):
-    url = "http://localhost:%d/api_v1/analyze" % web_port
-
-    resp = requests.post(url, data={"url": "http://kitakitsune.org/"})
+def test_rest(analyze_url):
+    resp = requests.post(analyze_url, data={"url": "http://kitakitsune.org/"})
     out = resp.json()
 
     assert out["status"]
+    assert "error" not in out
+    assert "body" in out
+
+    assert "all_set" in out["body"]
+    assert "progress" in out["body"]
+    assert "values" in out["body"]
+
+
+def test_rest_fail(analyze_url):
+    resp = requests.post(analyze_url, data={"url": "azgabash"})
+    out = resp.json()
+
+    assert not out["status"]
+    assert "error" in out
+    assert out["error"]
