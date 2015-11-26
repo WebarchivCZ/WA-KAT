a1aebac82db228eafc7858f59a898bacc192ab38
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-26 04:13:22 +0100

    #32: Added code for the progress bar.

--- src/wa_kat/templates/static/py/wa_kat_main.py
+++ src/wa_kat/templates/static/py/wa_kat_main.py
@@ -5,48 +5,81 @@
 #
 # Imports =====================================================================
 import json
+import time
 
 from browser import ajax
 from browser import alert  # TODO: Remove
 from browser import document
 
 
+# GUI views ===================================================================
+class UrlBoxError(object):
+    def __init__(self):
+        self.hide()
+
+    def show(self, msg):
+        document["urlbox_error"].innerHTML = msg
+        document["whole_urlbox_error"].style.display = "block"
+
+    def hide(self):
+        document["whole_urlbox_error"].style.display = "none"
+
+
+class ProgressBar(object):
+    def __init__(self):
+        # self.hide()
+        pass
+
+    def _compute_percentage(self, progress_tuple):
+        return (100 / progress_tuple[1]) * progress_tuple[0]
+
+    def show(self, progress, msg=None):
+        if document["whole_progress_bar"].style.display == "none":
+            document["whole_progress_bar"].style.display = "block"
+
+        percentage = str(self._compute_percentage(progress))
+        document["progress_bar"].aria_valuemin = percentage
+        document["progress_bar"].style.width = "{}%".format(percentage)
+
+        if msg:
+            document["progress_bar"].innerHTML = msg
+
+    def hide(self):
+        document["whole_progressbar"].style.display = "none"
+
+
 # Variables ===================================================================
+URL_BOX_ERROR = UrlBoxError()
+PROGRESS_BAR = ProgressBar()
+
+
 # Functions & classes =========================================================
-def show_url_box_error(msg):
-    document["urlbox_error"].innerHTML = msg
-    document["whole_urlbox_error"].style.display = "block"
-
-
-def hide_url_box_error():
-    document["whole_urlbox_error"].style.display = "none"
-
-
 def fill_inputs(values):
     alert(values)
 
 
-def show_progress(progress):
-    pass
-
-
 def on_complete(req):
+    # handle http errors
     if not (req.status == 200 or req.status == 0):
-        show_url_box_error(req.text)
+        URL_BOX_ERROR.show(req.text)
         return
 
     resp = json.loads(req.text)
 
+    # handle structured errors
     if not resp["status"]:
-        show_url_box_error(resp["error"])
+        URL_BOX_ERROR.show(resp["error"])
         return
 
+    # keep tracking of the progress
     if not resp["body"]["all_set"]:
-        show_progress(resp["body"]["progress"])
+        PROGRESS_BAR.show(resp["body"]["progress"])
+        time.sleep(0.5)
         make_request(document["url"].value)
         return
 
-    show_progress(resp["body"]["progress"])
+    # finally save the data to inputs
+    PROGRESS_BAR.show(resp["body"]["progress"])
     fill_inputs(resp["body"]["values"])
 
 
@@ -61,15 +94,17 @@ def make_request(url):
 def start_analysis(ev):
     url = document["url"].value.strip()
 
+    # make sure, that `url` was filled in
     if not url.strip():
-        show_url_box_error("URL musí být vyplněna.")
+        URL_BOX_ERROR.show("URL musí být vyplněna.")
         return
 
-    hide_url_box_error()
+    URL_BOX_ERROR.hide()
 
+    # normalize the `url`
     if not (url.startswith("http://") or url.startswith("http://")):
         url = "http://" + url
-        document["url"].value = url
+        document["url"].value = url  # store normalized url back to input
 
     make_request(url)
 
