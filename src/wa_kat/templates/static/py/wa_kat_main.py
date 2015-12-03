1045b75948d637d15e760021a00feed51006d477
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-03 06:30:50 +0100

    All classes rewritten to static classes.

--- src/wa_kat/templates/static/py/wa_kat_main.py
+++ src/wa_kat/templates/static/py/wa_kat_main.py
@@ -15,64 +15,69 @@ from browser import document
 
 # GUI views ===================================================================
 class UrlBoxError(object):
-    def __init__(self):
-        self.tag = document["urlbox_error"]
-        self.whole_tag = document["whole_urlbox_error"]
+    tag = document["urlbox_error"]
+    whole_tag = document["whole_urlbox_error"]
 
-    def show(self, msg):
-        self.tag.innerHTML = msg
-        self.whole_tag.style.display = "block"
+    @staticmethod
+    def show(msg):
+        UrlBoxError.tag.innerHTML = msg
+        UrlBoxError.whole_tag.style.display = "block"
 
-    def hide(self):
-        self.whole_tag.style.display = "none"
+    @staticmethod
+    def hide():
+        UrlBoxError.whole_tag.style.display = "none"
 
-    def reset(self):
-        self.hide()
-        self.tag.innerHTML = ""
+    @staticmethod
+    def reset():
+        UrlBoxError.hide()
+        UrlBoxError.tag.innerHTML = ""
 
 
 class ProgressBar(object):
-    def __init__(self):
-        self.tag = document["progress_bar"]
-        self.whole_tag = document["whole_progress_bar"]
-        self.original_message = self.tag.text
+    tag = document["progress_bar"]
+    whole_tag = document["whole_progress_bar"]
+    original_message = tag.text
 
-    def _compute_percentage(self, progress_tuple):
+    @staticmethod
+    def _compute_percentage(progress_tuple):
         # division by zero..
         if progress_tuple[0] == 0:
             return 0
 
         return (100 / progress_tuple[1]) * progress_tuple[0]
 
-    def show(self, progress, msg=None):
-        if self.whole_tag.style.display == "none":
-            self.whole_tag.style.display = "block"
+    @staticmethod
+    def show(progress, msg=None):
+        if ProgressBar.whole_tag.style.display == "none":
+            ProgressBar.whole_tag.style.display = "block"
 
-        percentage = self._compute_percentage(progress)
+        percentage = ProgressBar._compute_percentage(progress)
 
         # toggle animation
+        ProgressBar.tag.class_name = "progress-bar"
         if percentage < 100:
-            self.tag.class_name = "progress-bar progress-bar-striped active"
+            ProgressBar.tag.class_name += " progress-bar-striped active"
         else:
-            self.tag.class_name = "progress-bar"
             msg = "Hotovo"
 
         # show percentage in progress bar
-        self.tag.aria_valuemin = percentage
-        self.tag.style.width = "{}%".format(percentage)
+        ProgressBar.tag.aria_valuemin = percentage
+        ProgressBar.tag.style.width = "{}%".format(percentage)
 
         if msg:
-            self.tag.text = msg
+            ProgressBar.tag.text = msg
 
-    def hide(self):
-        self.whole_tag.style.display = "none"
+    @staticmethod
+    def hide():
+        ProgressBar.whole_tag.style.display = "none"
 
-    def reset(self):
-        self.hide()
-        self.tag.class_name = "progress-bar progress-bar-striped active"
-        self.tag.aria_valuemin = 0
-        self.tag.style.width = "{}%".format(0)
-        self.tag.text = self.original_message
+    @staticmethod
+    def reset():
+        ProgressBar.hide()
+        ProgressBar.tag.class_name = "progress-bar progress-bar-striped active"
+        ProgressBar.tag.aria_valuemin = 0
+        ProgressBar.tag.style.width = "{}%".format(0)
+        ProgressBar.tag.text = ProgressBar.original_message
 
 
 class DropdownHandler(object):
@@ -117,44 +122,49 @@ class DropdownHandler(object):
 
 
 class InputMapper(object):
-    def __init__(self):
-        self._map = {  # TODO: get rid of this
-            "title_tags": "title",
-            "place_tags": "place",
-            "lang_tags": "lang",
-            "keyword_tags": "keywords",
-            "author_tags": "author",
-            "annotation_tags": "annotation",
-            "creation_dates": "creation_date",
-        }
-        self._set_by_typeahead = set()
-        self._dropdown_text = " Klikněte pro výběr."
+    _map = {  # TODO: get rid of this
+        "title_tags": "title",
+        "place_tags": "place",
+        "lang_tags": "lang",
+        "keyword_tags": "keywords",
+        "author_tags": "author",
+        "annotation_tags": "annotation",
+        "creation_dates": "creation_date",
+    }
+    _set_by_typeahead = set()
+    _dropdown_text = " Klikněte pro výběr."
 
-    def _get_el(self, rest_id):
-        return document[self._map[rest_id]]
+    @staticmethod
+    def _get_el(rest_id):
+        return document[InputMapper._map[rest_id]]
 
-    def set_placeholder_text(self, input_el, text):
+    @staticmethod
+    def set_placeholder_text(input_el, text):
         input_el.placeholder = text
 
-    def get_placeholder_text(self, input_el):
-        # if not hasattr(input_el, "placeholder"):
-        #     return ""
-
+    @staticmethod
+    def get_placeholder_text(input_el):
         return input_el.placeholder
 
-    def set_placeholder_dropdown(self, input_el):
-        text = self.get_placeholder_text(input_el)
-        self.set_placeholder_text(input_el, text + self._dropdown_text)
-
-    def reset_placeholder_dropdown(self, input_el):
-        text = self.get_placeholder_text(input_el)
-        self.set_placeholder_text(
-            input_el,
-            text.replace(self._dropdown_text, "")
+    @staticmethod
+    def set_placeholder_dropdown(input_el):
+        text = InputMapper.get_placeholder_text(input_el)
+        InputMapper.set_placeholder_text(
+            input_el=input_el,
+            text=text + InputMapper._dropdown_text
         )
 
-    def _set_typeahead(self, key, el, value):
-        self.reset_placeholder_dropdown(el)
+    @staticmethod
+    def reset_placeholder_dropdown(input_el):
+        text = InputMapper.get_placeholder_text(input_el)
+        InputMapper.set_placeholder_text(
+            input_el=input_el,
+            text=text.replace(InputMapper._dropdown_text, "")
+        )
+
+    @staticmethod
+    def _set_typeahead(key, el, value):
+        InputMapper.reset_placeholder_dropdown(el)
 
         # if there is no elements, show alert icon in glyph
         if not value:
@@ -188,77 +198,76 @@ class InputMapper(object):
         # dropdown glyph
         window.make_typeahead_tag("#" + parent_id, value)
         DropdownHandler.set_dropdown_glyph(el.id, "glyphicon-menu-down")
-        self.set_placeholder_dropdown(el)
-        self._set_by_typeahead.add(parent_id)
+        InputMapper.set_placeholder_dropdown(el)
+        InputMapper._set_by_typeahead.add(parent_id)
 
-    def _set_input(self, key, el, value):
+    @staticmethod
+    def _set_input(key, el, value):
         el.value = ", ".join(item.val for item in value)
 
-    def _set_textarea(self, key, el, value):
+    @staticmethod
+    def _set_textarea(key, el, value):
         el.text = "\n\n".join(
             "-- %s --\n%s" % (item["source"], item["val"])
             for item in value
         )
 
-    def map(self, key, value):
-        el = self._get_el(key)
+    @staticmethod
+    def map(key, value):
+        el = InputMapper._get_el(key)
         tag_name = el.elt.tagName.lower()
 
-        if tag_name == "textarea":  # TODO: handle case when there already is something
-            self._set_textarea(key, el, value)
+        if tag_name == "textarea":
+            InputMapper._set_textarea(key, el, value)
         elif tag_name == "input":
             if "typeahead" in el.class_name.lower():
-                self._set_typeahead(key, el, value)
+                InputMapper._set_typeahead(key, el, value)
             else:
-                self._set_input(key, el, value)
+                InputMapper._set_input(key, el, value)
         elif tag_name == "select":
-            pass  #: TODO: implement selecting of the keywords
+            pass  # TODO: implement selecting of the keywords
         else:
             alert(
                 "Setter for %s (%s) not implemented!" % (tag_name, el.id)
             )
 
-    def fill_inputs(self, values):
+    @staticmethod
+    def fill_inputs(values):
         for key, value in values.items():
-            self.map(key, value)
+            InputMapper.map(key, value)
 
-    def reset(self):
-        for el_id in self._set_by_typeahead:
+    @staticmethod
+    def reset():
+        for el_id in InputMapper._set_by_typeahead:
             window.destroy_typyahead_tag("#" + el_id)
 
-        self._set_by_typeahead = set()
+        InputMapper._set_by_typeahead = set()
 
 
-# Variables ===================================================================
-URL_BOX_ERROR = UrlBoxError()
-PROGRESS_BAR = ProgressBar()
-INPUT_MAPPER = InputMapper()
-
-
-# Functions & classes =========================================================
+# Functions ===================================================================
 def on_complete(req):
     # handle http errors
     if not (req.status == 200 or req.status == 0):
-        URL_BOX_ERROR.show(req.text)
+        UrlBoxError.show(req.text)
         return
 
     resp = json.loads(req.text)
 
     # handle structured errors
     if not resp["status"]:
-        URL_BOX_ERROR.show(resp["error"])
+        UrlBoxError.show(resp["error"])
         return
 
     # keep tracking of the progress
     if not resp["body"]["all_set"]:
-        PROGRESS_BAR.show(resp["body"]["progress"])
+        ProgressBar.show(resp["body"]["progress"])
         time.sleep(0.5)
         make_request(document["url"].value)
         return
 
     # finally save the data to inputs
-    PROGRESS_BAR.show(resp["body"]["progress"])
-    INPUT_MAPPER.fill_inputs(resp["body"]["values"])
+    ProgressBar.show(resp["body"]["progress"])
+    InputMapper.fill_inputs(resp["body"]["values"])
 
 
 def make_request(url):
@@ -271,20 +280,20 @@ def make_request(url):
 
 def start_analysis(ev):
     # reset all inputs
-    PROGRESS_BAR.reset()
-    PROGRESS_BAR.show([0, 0])
-    INPUT_MAPPER.reset()
-    URL_BOX_ERROR.reset()
+    ProgressBar.reset()
+    ProgressBar.show([0, 0])
+    InputMapper.reset()
+    UrlBoxError.reset()
 
     # read the urlbox
     url = document["url"].value.strip()
 
     # make sure, that `url` was filled in
     if not url.strip():
-        URL_BOX_ERROR.show("URL musí být vyplněna.")
+        UrlBoxError.show("URL musí být vyplněna.")
         return
 
-    URL_BOX_ERROR.hide()
+    UrlBoxError.hide()
 
     # normalize the `url`
     if not (url.startswith("http://") or url.startswith("http://")):
