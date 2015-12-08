ef74e5096ddeffc7d79862111bc94c11d76d7a24
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-08 22:35:52 +0100

    #11: Fixed bugs in make_multi_searchable_typeahead_tag().

--- src/wa_kat/templates/static/js/typeahead_custom.js
+++ src/wa_kat/templates/static/js/typeahead_custom.js
@@ -26,25 +26,32 @@ var make_typeahead_tag = function(tag_id, hints){
 }
 
 var make_multi_searchable_typeahead_tag  = function(){
-  all_tags = [{
+  // start with basic typeahead settings
+  all_tags = new Array({
     hint: true,
     minLength: 0,
     highlight: true,
-  }];
+  });
 
+  // first argument is the ID of the tag which will be converted to typeahead
   tag_id = arguments[0];
+
+  // map arguments to `all_tags` array
   for (var i = 1; i < arguments.length; i++) {
     dataset = {
-      source: arguments[i].data,
+      limit: 20,
+      source: tag_passer(arguments[i].data),
       templates: {
-        header: '<h3 class="conspect_name">' arguments[i].name + '</h3>'
+        header: '<h3 class="conspect_name">' + arguments[i].name + '</h3>',
       }
-    };
+    }
 
     all_tags.push(dataset);
   }
 
-  $(tag_id + ' .typeahead').typeahead.apply(all_tags);
+  // convert the tag to typeahead with multiple searchable data sources
+  typeahead_tag = $(tag_id + ' .typeahead');
+  typeahead_tag.typeahead.apply(typeahead_tag, all_tags);
 }
 
 var destroy_typyahead_tag = function(tag_id){
