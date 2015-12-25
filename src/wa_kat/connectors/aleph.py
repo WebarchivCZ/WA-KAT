28a7800c892ee180813b867a3df91b1c7882eac4
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-25 22:52:06 +0100

    #31: Added _add_source(), attempt to handle keywords (#37).

--- src/wa_kat/connectors/aleph.py
+++ src/wa_kat/connectors/aleph.py
@@ -6,9 +6,11 @@
 # Imports =====================================================================
 from remove_hairs import remove_hairs
 from marcxml_parser import MARCXMLRecord
-from edeposit.amqp.aleph import aleph
+# from edeposit.amqp.aleph import aleph
+import local_aleph as aleph
 
 from ..data_model import Model
+from ..analyzers.source_string import SourceString
 
 
 # Variables ===================================================================
@@ -20,23 +22,40 @@ def _first_or_none(array):
     return array[0]
 
 
+def _add_source(model):
+    # convert all values to source strings
+    source = "Aleph"
+    for key, val in model.get_mapping().iteritems():
+        if type(val) in [list, tuple]:
+            ss_val = [
+                SourceString(item, source).to_dict()
+                for item in val
+            ]
+        else:
+            ss_val = [SourceString(val, source).to_dict()]
+
+        setattr(model, key, ss_val)
+
+    return model
+
+
 def by_issn(issn):
     for record in aleph.getISSNsXML(issn):
         marc = MARCXMLRecord(record)
 
         model = Model(
-            url=_first_or_none(
-                marc.get("856u")
-            ),
-            conspect=_first_or_none(
-                marc.get("072a")
-            ),
+            # url=_first_or_none(
+            #     marc.get("856u")
+            # ),
+            # conspect=_first_or_none(
+            #     marc.get("072a")
+            # ),
             annotation_tags=_first_or_none(
                 marc.get("520a")
             ),
-            periodicity=_first_or_none(
-                marc.get("310a")
-            ),
+            # periodicity=_first_or_none(
+            #     marc.get("310a")
+            # ),
             title_tags=_first_or_none(
                 marc.get("222a")
             ),
@@ -58,4 +77,17 @@ def by_issn(issn):
             ),
         )
 
-        yield model
+        yield _add_source(model)
+
+
+def resolve_keyword(keyword):
+    aleph.searchInAleph("aut10", keyword, True, "sh")
+    # keywords = aleph.downloadRecords(
+    #     aleph.searchInAleph("aut", "software", True, "kw")
+    # )
+
+    # for cnt, keyword in enumerate(keywords):
+    #     with open("/tmp/aleph/%d.xml" % cnt, "w") as f:
+    #         f.write(keyword)
+
+resolve_keyword("software")
