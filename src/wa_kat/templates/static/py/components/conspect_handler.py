1ef49bc4595d70266e5139685680bd3d76c6a7ce
Bystroushaak <bystrousak@kitakitsune.org>
2016-01-20 19:45:55 +0100

    Fixed #44; bug in conspect's typeahead.

--- src/wa_kat/templates/static/py/components/conspect_handler.py
+++ src/wa_kat/templates/static/py/components/conspect_handler.py
@@ -60,6 +60,7 @@ class ConspectHandler(object):
         """
         Create typeahead <input> element and fill it with data.
         """
+        window.destroy_typyahead_tag("#conspect_subconspect_typeahead")
         args = [
             {
                 "name": key,
