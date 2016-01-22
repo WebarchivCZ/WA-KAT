dc58f52a33ae3bcf6a4b947409659f886591f276
Bystroushaak <bystrousak@kitakitsune.org>
2016-01-22 19:30:29 +0100

    Fixed bugs in keyword handler's compression.

--- src/wa_kat/rest_api/keywords.py
+++ src/wa_kat/rest_api/keywords.py
@@ -10,9 +10,11 @@ import os.path
 from os.path import join
 
 from bottle import get
+from bottle import response
 
 from shared import API_PATH
 from shared import gzip_cache
+from shared import RESPONSE_TYPE
 from shared import to_gzipped_file
 
 
@@ -26,26 +28,29 @@ def read_kw_file():
     with bz2.BZ2File(kw_list_path) as f:
         kw_list = f.read()
 
-    return kw_list
+    return json.loads(kw_list)
 
 
 # Variables ===================================================================
 KW_DICT = read_kw_file()
 KEYWORDS = [
-    keyword_dict["zahlavi"]
-    for keyword_dict in json.loads(KW_DICT)
+    keyword_dict["zahlavi"].encode("utf-8")
+    for keyword_dict in KW_DICT
 ]
+KEYWORDS_JSON = json.dumps(KEYWORDS)
 KW_CACHE_PATH = "/tmp/wa_kat_cache_keywords.json"
 
 # create cached files
 with open(KW_CACHE_PATH, "w") as f:
-    f.write(KW_DICT)
+    f.write(KEYWORDS_JSON)
 with open(KW_CACHE_PATH + ".gz", "w") as f:
-    to_gzipped_file(KW_DICT, out=f)
+    to_gzipped_file(KEYWORDS_JSON, out=f)
 
 
 # Functions ===================================================================
 # API =========================================================================
 @get(join(API_PATH, "kw_list.json"))
 def get_kw_list():
+    response.content_type = RESPONSE_TYPE
+
     return gzip_cache(KW_CACHE_PATH)
