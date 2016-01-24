ac6cad9d7d896f810f00e4e8bbff822c19d95255
Bystroushaak <bystrousak@kitakitsune.org>
2016-01-24 19:05:31 +0100

    #12: Added partialy-done function for output conversion.

--- src/wa_kat/rest_api/to_marc.py
+++ src/wa_kat/rest_api/to_marc.py
@@ -11,16 +11,77 @@ from os.path import join
 from marcxml_parser import MARCXMLRecord
 
 from bottle import post
+from bottle import SimpleTemplate
 from bottle_rest import form_to_params
 
 from shared import API_PATH
+from keywords import keyword_to_info
 
 
 # Variables ===================================================================
 # Functions & classes =========================================================
+def template_context(fn):
+    return os.path.join(
+        os.path.dirname(__file__),
+        "../templates/",
+        fn
+    )
+
+
+def template(fn):
+    with open(template_context(fn)) as f:
+        return f.read()
+
+
+def compile_keywords(keywords):
+    cz_keywords = []
+    en_keywords = []
+    for keyword in keywords:
+        keyword = keyword_to_info(keyword)
+
+        if not keyword:
+            continue
+
+        cz_keywords.append(
+            {
+                "uid": keyword["uid"],
+                "zahlavi": keyword["zahlavi"],
+                "zdroj": "czenas",
+            }
+        )
+        if "angl_ekvivalent" in keyword:
+            en_keywords.append({
+                "zahlavi": keyword["angl_ekvivalent"],
+                "zdroj": keyword.get("zdroj_angl_ekvivalentu") or "eczenas",
+            })
+
+    return cz_keywords, en_keywords
+
+
+def render_mrc(data):
+    template_body = template("sablona_katalogizace_eperiodika.mrc")
+
+    return SimpleTemplate(template_body).render(**data)
+
+
 @post(join(API_PATH, "to_marc"))
 @form_to_params
 def to_marc(data):
     data = json.loads(data)
+
+    if "keywords" in data:
+        cz_keywords, en_keywords = compile_keywords(data["keywords"])
+        del data["keywords"]
+
+        data["cz_keywords"] = cz_keywords
+        data["en_keywords"] = en_keywords
+
+    data["annotation"] = data["annotation"].replace("\n", " ")
+
+    out = {
+        "mrc": render_mrc(data),
+    }
+
+    # with open("name")
 
     return "ok"
