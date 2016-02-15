b894a3a27e9492871157237bab13b21af3025764
Bystroushaak <bystrousak@kitakitsune.org>
2016-02-15 17:18:19 +0100

    #22: Fixed language test.

--- tests/analyzers/test_language_detector.py
+++ tests/analyzers/test_language_detector.py
@@ -30,11 +30,11 @@ Zde je nějaký ten obsah, který by měl být rozpoznaný jako čeština.
 def test_get_lang_tags():
     lang_tags = get_lang_tags(TEST_TEMPLATE)
 
-    assert lang_tags[0] == "cs"
+    assert lang_tags[0] == "cze"
     assert lang_tags[0].source == "HTML"
 
-    assert lang_tags[1] == "cs"
+    assert lang_tags[1] == "cze"
     assert lang_tags[1].source == "DC"
 
-    assert lang_tags[2] == "cs"
+    assert lang_tags[2] == "cze"
     assert lang_tags[2].source == "langdetect"
