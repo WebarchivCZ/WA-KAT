20e07813cf0423f9dd309e971ff99ca8e83870fa
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-09 21:09:08 +0100

    #32: settings.py are now available in virtual fs.

--- src/wa_kat/rest_api/virtual_fs.py
+++ src/wa_kat/rest_api/virtual_fs.py
@@ -13,6 +13,8 @@ from os.path import join
 from bottle import get
 from bottle import response
 
+from .. import settings
+
 from shared import in_template_path
 
 
@@ -34,7 +36,32 @@ def python_mime(fn):
 # API =========================================================================
 @get(in_virtual_path("__init__.py"))
 @python_mime
-def init():
+def init_api():
+    return """#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+    """
+
+
+@get(in_virtual_path("settings.py"))
+@python_mime
+def settings_api():
+    out = "\n"
+
+    for var in sorted(dir(settings)):
+        if var.startswith("_") or var.upper() != var:
+            continue
+
+        out += "%s = %s\n\n" % (var, repr(getattr(settings, var)))
+
+    return """#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#""" + out
+
+
+# http://localhost:8080/static/js/Lib/site-packages/virtual_fs/__init__.py
+@get(in_virtual_path("conspectus.py"))
+@python_mime
+def conspectus_api():
     return """#! /usr/bin/env python
 # -*- coding: utf-8 -*-
     """
