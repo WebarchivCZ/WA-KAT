c6b1450ce55437bab9eb8745642bb9d4db8b44ab
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-13 03:24:35 +0100

    #66: mrc_to_marc.py renamed to mrc.py, because new functions were added.

--- src/wa_kat/convertors/__init__.py
+++ src/wa_kat/convertors/__init__.py
@@ -4,4 +4,4 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-from mrc_to_marc import mrc_to_marc
+from mrc import mrc_to_marc
