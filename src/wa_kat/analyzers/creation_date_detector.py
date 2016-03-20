1000547277536cc9b0ddf685cedafbee60389c98
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-20 02:48:40 +0100

    #4: Implemented parsing of resources from the MementoWeb.org.

--- src/wa_kat/analyzers/creation_date_detector.py
+++ src/wa_kat/analyzers/creation_date_detector.py
@@ -4,13 +4,59 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-
+import requests
+from kwargs_obj import KwargsObj
 
 
 # Variables ===================================================================
+class TimeResource(KwargsObj):
+    def __init__(self, **kwargs):
+        self.url = None
+        self.date = None
 
+        # for backward compatibility with SourceString
+        self.val = None
+        self.source = None
+
+        self._kwargs_to_attributes(kwargs)
+
+    def _as_dict(self):
+        return {
+            key: val
+            for key, val in self.__dict__.iteritems()
+        }
+
+    def __str__(self):
+        return self.val
+
+    def __repr__(self):
+        return "%s(%s)" % (self.source, self.val)
 
 
 # Functions & classes =========================================================
+def mementoweb_api(url):
+    memento_url = "http://labs.mementoweb.org/timemap/json/"
+
+    r = requests.get(memento_url + url)  # TODO: error handling
+
+    data = r.json().get("mementos", {}).get("list", [])
+
+    if not data:
+        return []
+
+    return [
+        TimeResource(
+            url=item.get("uri", ""),
+            date=item.get("datetime", ""),
+            val=item.get("datetime", "").split("-")[0],
+            source="MementoWeb.org",
+        )
+        for item in data
+    ]
+
+
 def get_creation_date_tags(url, domain):
     return []
+
+
+print mementoweb_api("http://kitakitsune.org")
