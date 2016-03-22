8025f2b24d82a71a709cdd9c2b524c4842282036
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-22 05:42:15 +0100

    #2: Added better unicode decoder for analyzers.

--- src/wa_kat/zeo/request_info.py
+++ src/wa_kat/zeo/request_info.py
@@ -105,7 +105,24 @@ class RequestInfo(Persistent):
             verify=False,
         )
 
-        return resp.text.encode("utf-8")  # TODO: what about binaries?
+        def decode(st, alt_encoding=None):
+            encodings = ['ascii', 'utf-8', 'iso-8859-1', 'iso-8859-15']
+
+            if alt_encoding:
+                if isinstance(alt_encoding, basestring):
+                    encodings.append(alt_encoding)
+                else:
+                    encodings.extend(alt_encoding)
+
+            for encoding in encodings:
+                try:
+                    return st.encode(encoding).decode("utf-8")
+                except UnicodeEncodeError, UnicodeDecodeError:
+                    pass
+
+            raise UnicodeError('Could not find encoding.')
+
+        return decode(resp.text, resp.encoding)
 
     @transaction_manager
     def log(self, msg):
