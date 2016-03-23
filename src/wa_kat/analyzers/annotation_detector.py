6fa6437eee52ec2b28cdd78968187063640d6df8
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-23 07:13:38 +0100

    #25: Added docstrings to annotation_detector.py.

--- src/wa_kat/analyzers/annotation_detector.py
+++ src/wa_kat/analyzers/annotation_detector.py
@@ -3,6 +3,11 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+Parse `annotation` (short comment about page) from HTML ``<meta>`` and
+`Dublin core <http://dublincore.org>`_ tags.
+"""
+#
 # Imports =====================================================================
 import dhtmlparser
 
@@ -10,16 +15,28 @@ from .shared import parse_meta
 
 
 # Functions & classes =========================================================
-def _get_html_annotations(index_page):
+def get_html_annotations(index_page):
     """
     Return `descriptions` stored in ``<meta>`` tags.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "description", "Meta")
 
 
-def _get_dc_annotations(index_page):
+def get_dc_annotations(index_page):
     """
     Return `description` stored in dublin core ``<meta>`` tags.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "dc.description", "DC")
 
@@ -38,8 +55,8 @@ def get_annotation_tags(index_page):
     dom = dhtmlparser.parseString(index_page)
 
     descriptions = [
-        _get_html_annotations(dom),
-        _get_dc_annotations(dom),
+        get_html_annotations(dom),
+        get_dc_annotations(dom),
     ]
 
     return sum(descriptions, [])  # return flattened list
