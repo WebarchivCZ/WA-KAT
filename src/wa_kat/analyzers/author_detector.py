cce3fee8cf0e15e3ef8b8d1fc43b0ef7d0f831de
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-23 07:11:44 +0100

    #25: Added docstrings to author_detector.py.

--- src/wa_kat/analyzers/author_detector.py
+++ src/wa_kat/analyzers/author_detector.py
@@ -3,6 +3,11 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+Parse informations about authors from HTML ``<meta>`` tags and
+`Dublin core <http://dublincore.org>`_.
+"""
+#
 # Imports =====================================================================
 import dhtmlparser
 
@@ -10,16 +15,28 @@ from shared import parse_meta
 
 
 # Functions & classes =========================================================
-def _get_html_authors(index_page):
+def get_html_authors(index_page):
     """
     Return list of `authors` from HTML ``<meta>`` tags.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "author", "HTML")
 
 
-def _get_dc_authors(index_page):
+def get_dc_authors(index_page):
     """
     Return list of `authors` parsed from dublin core.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "DC.Creator", "DC")
 
@@ -27,12 +44,18 @@ def _get_dc_authors(index_page):
 def get_author_tags(index_page):
     """
     Parse `authors` from HTML ``<meta>`` and dublin core.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     dom = dhtmlparser.parseString(index_page)
 
     authors = [
-        _get_html_authors(dom),
-        _get_dc_authors(dom),
+        get_html_authors(dom),
+        get_dc_authors(dom),
     ]
 
     return sum(authors, [])  # return flattened list
