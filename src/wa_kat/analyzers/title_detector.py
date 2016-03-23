a95db71fafa9203c15a75848dba9667cf13ea7f4
Bystroushaak <bystrousak@kitakitsune.org>
2016-03-23 06:52:08 +0100

    #25: Added docstrings to title_detector.py.

--- src/wa_kat/analyzers/title_detector.py
+++ src/wa_kat/analyzers/title_detector.py
@@ -3,6 +3,10 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+This module is used to detect titles of the webpages from several sources.
+"""
+#
 # Imports =====================================================================
 import dhtmlparser
 
@@ -11,9 +15,15 @@ from source_string import SourceString
 
 
 # Functions & classes =========================================================
-def _get_html_titles(index_page):
+def get_html_titles(index_page):
     """
     Return list of titles parsed from HTML.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     dom = dhtmlparser.parseString(index_page)
 
@@ -26,38 +36,51 @@ def _get_html_titles(index_page):
     ]
 
 
-def _get_html_meta_titles(index_page):
+def get_html_meta_titles(index_page):
     """
     Return list of titles parsed from ``<meta>`` tags.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "title", "Meta")
 
 
-def _get_dublin_core_titles(index_page):
+def get_dublin_core_titles(index_page):
     """
     Return list of titles parsed from dublin core inlined in ``<meta>``
     tags.
+
+    Args:
+        index_page (str): HTML content of the page you wisht to analyze.
+
+    Returns:
+        list: List of :class:`.SourceString` objects.
     """
     return parse_meta(index_page, "dc.title", "DC")
 
 
 def get_title_tags(index_page):
     """
-    Return list of titles parsed from HTML, ``<meta>`` tags and dublin core
-    inlined in ``<meta>`` tags.
+    Collect data from all the functions defined in this module and return list
+    of titles parsed from HTML, ``<meta>`` tags and dublin core inlined in
+    ``<meta>`` tags.
 
     Args:
         index_page (str): HTML content of the page you wisht to analyze.
 
     Returns:
-        list: List of ``SourceString`` objects.
+        list: List of :class:`.SourceString` objects.
     """
     dom = dhtmlparser.parseString(index_page)
 
     titles = [
-        _get_html_titles(dom),
-        _get_html_meta_titles(dom),
-        _get_dublin_core_titles(dom),
+        get_html_titles(dom),
+        get_html_meta_titles(dom),
+        get_dublin_core_titles(dom),
     ]
 
     return sum(titles, [])  # return flattened list
