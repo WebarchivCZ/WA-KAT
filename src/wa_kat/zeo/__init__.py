14ae945eb1b53e057636d6e170daa886760e9a9f
Bystroushaak <bystrousak@kitakitsune.org>
2016-12-17 18:14:47 +0100

    #86: ZEO removed.

--- src/wa_kat/zeo/__init__.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Interpreter version: python 2.7
-#
-# Imports =====================================================================
-from request_database import RequestDatabase
