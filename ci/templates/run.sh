290d3267b76aa35290f5cd5881e1654a3f89d541
Rudolf Kreibich <rudolf.kreibich@merck.com>
2021-01-10 22:59:12 +0100

    Deployment pipeline MAINTAINER is deprectated Pipeline use prebuilt images by Jiří Horák

---
+++ ci/templates/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Make Bash Great Again
+set -o errexit # exit when a command fails.
+set -o nounset # exit when using undeclared variables
+set -o pipefail # catch non-zero exit code in pipes
+# set -o xtrace # uncomment for bug hunting
+
+docker pull webarchiv/kat:{{docker_tag}}
+docker rm -f wa-kat >/dev/null 2>&1 || /bin/true
+
+docker run --restart=always -d \
+ --name wa-kat \
+ --label "traefik.http.routers.kat.rule=Host(\`kat.webarchiv.cz\`)" \
+ webarchiv/kat:{{docker_tag}}
+
+ 
